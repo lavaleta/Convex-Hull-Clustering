faba51a807a836addce555aa8ff477d44487e84a
lavaleta <aminic17@raf.rs>
2020-01-15 17:27:27 +0100

    Final commit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Lavaleta\PycharmProjects\Projekat_Geometrijski\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
